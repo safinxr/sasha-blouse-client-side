7613e3abc39e1661cf61a5543e532957691eab3f
safinxr <safinxr@gmail.com>
2023-11-05 00:39:20 +0600

    navbar added

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,41 +1,63 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './navbar.css'
 
 const Navbar = () => {
+
+    const navLink = <>
+        <li><NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "mx-2 relative active-underline" : "mx-2 relative nav-underline"
+            }
+        >
+            HOME
+        </NavLink></li>
+        <li><NavLink
+            to="/fooditems"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "mx-2 relative active-underline" : "mx-2 relative nav-underline"
+            }
+        >
+            Food Items
+        </NavLink></li>
+        <li><NavLink
+            to="/blog"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "mx-2 relative active-underline" : "mx-2 relative nav-underline"
+            }
+        >
+            Blog
+        </NavLink></li>
+    </>
+    const smNavLink = <>
+        <li><NavLink to='/'>HOME</NavLink></li>
+        <li><NavLink to='/'>HOME</NavLink></li>
+        <li><NavLink to='/'>HOME</NavLink></li>
+        <li><NavLink to='/'>HOME</NavLink></li>
+
+    </>
     return (
-        <nav>
-            <div className="navbar bg-base-100">
+        <nav className='bg-gray-300 '>
+            <div className="navbar max-w-6xl mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {smNavLink}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <Link to='/'><div className='flex items-center'>
+                        <img className='w-14' src="https://i.ibb.co/rMkd0wL/chef.png" alt="" />
+                        <p className='agbalumo leading-5 text-xl black-text'>Sasha <br /> Blouse</p>
+
+                    </div></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul className=" menu-horizontal px-1 text-base font-semibold black-text uppercase tracking-widest">
+                        {navLink}
                     </ul>
                 </div>
                 <div className="navbar-end">
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home/Home";
+import Blog from "../Pages/Blog/Blog";
+import FoodItems from "../Pages/FoodItems/FoodItems";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
             {
               path:'/',
               element:<Home></Home>  
+            },
+            {
+              path:'/blog',
+              element:<Blog></Blog> 
+            },
+            {
+              path:'/fooditems',
+              element:<FoodItems></FoodItems>
             },
         ]
     },
