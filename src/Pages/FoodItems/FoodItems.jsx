43a6bb88d4442bd32cf041fa3fd665bd0c6b597e
safinxr <safinxr@gmail.com>
2023-11-07 18:28:40 +0600

    all food items page done

--- src/Pages/FoodItems/FoodItems.jsx
+++ src/Pages/FoodItems/FoodItems.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import AllFoodCard from './AllFoodCard';
 import { PropagateLoader } from 'react-spinners';
+import { HiOutlineEmojiSad } from 'react-icons/hi';
 
 const FoodItems = () => {
     const [pageCount, setPageCount] = useState(1)
@@ -12,7 +13,7 @@ const FoodItems = () => {
     const [allData, setAllData] = useState([]);
     const [search, setSearch] = useState('')
     const [dataCount, setDataCount] = useState(0)
-    console.log(dataCount);
+    const [inputValue, setInputValue] = useState(true)
 
 
 
@@ -30,26 +31,35 @@ const FoodItems = () => {
 
     const searchF = e => {
         setSearch(e.target.value.toLowerCase());
+        setInputValue(true)
     }
     const searchBtn = () => {
-        setLoading(true)
-        console.log(search);
-        axios.get(`http://localhost:5000/searchfood/?name=${search}&`)
-            .then(res => {
-                setAllData(res.data)
-                setSearchPage(res.data.length)
-                setLoading(false)
-            })
+        if(search){
+            setLoading(true)
+            console.log(search);
+            axios.get(`http://localhost:5000/searchfood/?name=${search}&`)
+                .then(res => {
+                    setAllData(res.data)
+                    setSearchPage(res.data.length)
+                    setLoading(false)
+                })
+        }
+        else{
+            setInputValue(false)
+        }
     }
 
     return (
         <div className='max-w-6xl mx-auto px-3 md:-px-8 lg:px-0'>
-            <div className='mt-12 text-2xl max-w-4xl mx-auto rounded-md font-medium border-2  border-[#231F20] flex items-center'>
+            <div className={inputValue ? 'mt-12 text-2xl max-w-4xl mx-auto rounded-md font-medium border-2  border-[#231F20] flex items-center':
+                'mt-12 text-2xl max-w-4xl mx-auto rounded-md font-medium border-2  border-[#eb2d2d] flex items-center'
+            }>
                 <div className='px-4'>
                     <BiSearchAlt2></BiSearchAlt2>
                 </div>
 
                 <input
+                    required
                     onChange={searchF}
                     name='search'
                     className=' w-full h-full focus:outline-none bg-transparent'
@@ -65,11 +75,17 @@ const FoodItems = () => {
                     <PropagateLoader color="#231F20" size={20} />
                 </div>
                     :
-                    <div className='my-12 md:my-16 lg:my-28 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                    allData.length > 0 ? <div className='my-12 md:my-16 lg:my-28 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
                         {
                             allData.map(food => <AllFoodCard food={food} key={food._id}></AllFoodCard>)
                         }
                     </div>
+                    :
+                    <div className='flex justify-center items-center h-[50vh]'>
+                            <h2 className='text-4xl text-gray-500  font-semibold uppercase'>no food found </h2>
+                            <h2 className='text-5xl text-gray-500  font-semibold uppercase ms-2'><HiOutlineEmojiSad></HiOutlineEmojiSad></h2>
+                    </div>
+                    
 
             }
 
