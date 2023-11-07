059e8c479a524f549c881b02bdcc2ece0fd77c90
safinxr <safinxr@gmail.com>
2023-11-08 00:58:01 +0600

    seacrh on inter added

--- src/Pages/FoodItems/FoodItems.jsx
+++ src/Pages/FoodItems/FoodItems.jsx
@@ -49,6 +49,12 @@ const FoodItems = () => {
         }
     }
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            searchBtn();
+        }
+    };
+
     return (
         <div className='max-w-6xl mx-auto px-3 md:-px-8 lg:px-0'>
             <div className={inputValue ? 'mt-12 text-2xl max-w-4xl mx-auto rounded-md font-medium border-2  border-[#231F20] flex items-center':
@@ -61,6 +67,7 @@ const FoodItems = () => {
                 <input
                     required
                     onChange={searchF}
+                    onKeyDown={handleKeyDown}
                     name='search'
                     className=' w-full h-full focus:outline-none bg-transparent'
                     type="text" placeholder='Type to search' />
