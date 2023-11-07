91249b05cee5fbe6917bff2592d7eff04f2cb5d5
safinxr <safinxr@gmail.com>
2023-11-08 05:34:49 +0600

    MY ADDED FOOD page ui done

--- src/Pages/MyAddedFood/MyAddedFood.jsx
+++ src/Pages/MyAddedFood/MyAddedFood.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { ContextAuth } from '../../Context/Context';
+import axios from 'axios';
+import Table from './Table';
 
 const MyAddedFood = () => {
+
+    const { user } = useContext(ContextAuth)
+    const [data, setData] = useState([])
+    
+
+
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/myaddedfood/?email=${user.email}`)
+        .then(res => setData(res.data))
+    },[])
+
+    console.log(data);
     return (
-        <div>
+        <div className='max-w-6xl mx-auto px-3 mx:px-8 lg:px-0'>
+            <h1 className='uppercase text-2xl font-bold text-center mt-16'>My added FooD</h1>
             
+            <div className="overflow-x-auto mt-6 shadow-2xl">
+                {
+                    data.map(food => <Table food={food} key={food._id}></Table>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/MyAddedFood/Table.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiFillDelete } from 'react-icons/ai';
+
+const Table = ({food}) => {
+    const { _id, food_name, food_image, food_category, price, quantity, added_by, food_origin, description, ordered } = food
+    return (
+        <table className="table">
+            <tbody>
+                <tr>
+                    <td className='w-5/12'>
+                        <div className="flex items-center space-x-3">
+                            <div className="avatar">
+                                <div className="mask rounded w-16 h-16">
+                                    <img src={food_image} alt="" />
+                                </div>
+                            </div>
+                            <div>
+                                <div className="font-bold uppercase text-lg">{food_name}</div>
+                                <div className=" opacity-70">Category : {food_category}</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td className='w-3/12'>
+                        Food Added by : {added_by}
+                        <br />
+                        <span className="badge badge-ghost badge-sm">
+                        Food Origin : {food_origin}</span>
+                    </td>
+                    <td className='w-2/12'>PRICE : ${price}
+                    <br/>
+                        Available quantity : {quantity}
+                    </td>
+                    <th className='w-2/12'>
+                        <div className='flex items-center justify-between'>
+                            <Link to='/' className='uppercase active:scale-95 px-4 bg-black py-2 text-white rounded'>update</Link>
+                            <button className="active:scale-95 text-2xl px-4 py-1 border-2 border-black rounded"><AiFillDelete></AiFillDelete></button>
+                        </div>
+
+                    </th>
+                </tr>
+            </tbody>
+
+
+        </table>
+    );
+};
+
+export default Table;
