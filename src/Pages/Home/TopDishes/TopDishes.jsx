0c59fdffb32f5812862b596328e784e7a85ad4c7
safinxr <safinxr@gmail.com>
2023-11-07 23:07:50 +0600

    top food part fully done

--- src/Pages/Home/TopDishes/TopDishes.jsx
+++ src/Pages/Home/TopDishes/TopDishes.jsx
@@ -23,17 +23,17 @@ const TopDishes = () => {
     return (
         <div className='max-w-6xl mx-auto px-3 md:px-8 lg:px-0 my-28'>
             <div>
-                <div className='flex justify-between items-start'>
-                    <h1 className='font-bold text-4xl black-text uppercase mb-4 '>top dishes</h1>
-                    <Link to='fooditems' className='px-4 py-1.5 border-2 border-[#231F20] flex items-center uppercase font-medium hover:bg-[#231F20]
-                     rounded hover:text-white duration-300'>See All Items <span className='ms-2 text-xl'><BsArrowRight></BsArrowRight></span></Link>
+                <div className='flex justify-between items-start '>
+                    <h1 className='font-bold text-2xl md:text-3xl lg:text-4xl black-text uppercase mb-4 '>top dishes</h1>
+                    <Link to='fooditems' className='px-2 py-1 md:px-4 md:py-1.5 border-2 border-[#231F20] md:flex items-center uppercase md:font-medium hover:bg-[#231F20]
+                     rounded hover:text-white duration-300 hidden'>See All Items <span className='ms-2 md:text-xl'><BsArrowRight></BsArrowRight></span></Link>
                 </div>
                 {
                     loading ? <div className='flex justify-center mt-32'>
                         <PropagateLoader color="#231F20" size={20} />
                     </div>
                     :
-                        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6'>
                             {
                                 topFood.map(food => <TopDishesCard food={food} key={food._id}></TopDishesCard>)
                             }
--- src/Pages/Home/TopDishes/TopDishesCard.jsx
+++ src/Pages/Home/TopDishes/TopDishesCard.jsx
@@ -4,21 +4,21 @@ import { AiFillStar } from 'react-icons/ai';
 const TopDishesCard = ({ food }) => {
     const { id, food_name, food_image, food_category, price, quantity, added_by, food_origin, description, ordered } = food
     return (
-        <div className='black-shadow rounded flex flex-col group'>
-            <div className='h-80 overflow-hidden'>
-                <img className='w-full h-full object-cover object-left rounded-t group-hover:scale-105 duration-300' src={food_image} alt="" />
+        <div className='black-shadow rounded flex md:flex-col group'>
+            <div className='h-32 md:h-80 w-40 md:w-auto overflow-hidden flex-1 md:flex-auto'>
+                <img className='w-full h-full object-cover md:object-left rounded-t group-hover:scale-105 duration-300' src={food_image} alt="" />
             </div>
 
             {/* 🤖🤖🤖🤖🤖🤖🤖 */}
 
 
-            <div className=' px-2 py-6 flex-grow flex flex-col justify-between '>
+            <div className=' px-2 py-6 flex-grow flex flex-col justify-between flex-1 md:flex-auto'>
                 
-                <h2 className='text-lg font-bold uppercase text-center line-clamp-1'>{food_name}</h2>
+                <h2 className='text-sm md:text-lg font-bold uppercase text-center line-clamp-2 md:line-clamp-1'>{food_name}</h2>
 
                 <div className=''>
-                    <p className=' font-medium uppercase text-center'>{food_category} || ${price}</p>
-                    <div className='text-xl flex justify-center items-center mt-2'>
+                    <p className=' font-medium uppercase text-center text-xs md:text-base'>{food_category} || ${price}</p>
+                    <div className='md:text-xl flex justify-center items-center mt-2'>
                         <AiFillStar></AiFillStar>
                         <AiFillStar></AiFillStar>
                         <AiFillStar></AiFillStar>
