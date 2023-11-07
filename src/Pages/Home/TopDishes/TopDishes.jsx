b1a8e371ad80c2e3aa0de4893d5ff8f668716695
safinxr <safinxr@gmail.com>
2023-11-07 22:21:12 +0600

    top dishes part done

--- src/Pages/Home/TopDishes/TopDishes.jsx
+++ src/Pages/Home/TopDishes/TopDishes.jsx
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import TopDishesCard from './TopDishesCard';
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
+import { PropagateLoader } from 'react-spinners';
 
 const TopDishes = () => {
+    const [loading, setLoading] = useState(true)
     const [topFood, setTopFood] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:5000/topdishes')
-            .then(res => setTopFood(res.data))
+            .then(res => {
+                setTopFood(res.data)
+                setLoading(false)
+            })
+            
     }, [])
 
 
@@ -15,12 +23,23 @@ const TopDishes = () => {
     return (
         <div className='max-w-6xl mx-auto px-3 md:px-8 lg:px-0 my-28'>
             <div>
-                <h1 className='font-bold text-4xl black-text text-center uppercase mb-10'>top dishes</h1>
-                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-                    {
-                        topFood.map(food => <TopDishesCard food={food} key={food._id}></TopDishesCard>)
-                    }
+                <div className='flex justify-between items-start'>
+                    <h1 className='font-bold text-4xl black-text uppercase mb-4 '>top dishes</h1>
+                    <Link to='fooditems' className='px-4 py-1.5 border-2 border-[#231F20] flex items-center uppercase font-medium hover:bg-[#231F20]
+                     rounded hover:text-white duration-300'>See All Items <span className='ms-2 text-xl'><BsArrowRight></BsArrowRight></span></Link>
                 </div>
+                {
+                    loading ? <div className='flex justify-center mt-32'>
+                        <PropagateLoader color="#231F20" size={20} />
+                    </div>
+                    :
+                        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                            {
+                                topFood.map(food => <TopDishesCard food={food} key={food._id}></TopDishesCard>)
+                            }
+                        </div>
+                }
+                
             </div>
         </div>
     );
--- src/Pages/Home/TopDishes/TopDishesCard.jsx
+++ src/Pages/Home/TopDishes/TopDishesCard.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { FaBeer } from 'react-icons/fa';
+import { AiFillStar } from 'react-icons/ai';
 
 const TopDishesCard = ({ food }) => {
     const { id, food_name, food_image, food_category, price, quantity, added_by, food_origin, description, ordered } = food
     return (
-        <div className='shadow-lg rounded'>
-            <div className='h-80'>
-                <img className='w-full h-full object-cover object-left rounded-t' src={food_image} alt="" />
+        <div className='black-shadow rounded flex flex-col group'>
+            <div className='h-80 overflow-hidden'>
+                <img className='w-full h-full object-cover object-left rounded-t group-hover:scale-105 duration-300' src={food_image} alt="" />
             </div>
-            <div className=' p-5'>
-                <h2 className='text-lg font-bold uppercase text-center '>{food_name}</h2>
-                <div>
-                    <p className='text-base font-medium uppercase text-center'>{food_category} / ${price}</p>
+
+            {/* 🤖🤖🤖🤖🤖🤖🤖 */}
+
+
+            <div className=' px-2 py-6 flex-grow flex flex-col justify-between '>
+                
+                <h2 className='text-lg font-bold uppercase text-center line-clamp-1'>{food_name}</h2>
+
+                <div className=''>
+                    <p className=' font-medium uppercase text-center'>{food_category} || ${price}</p>
+                    <div className='text-xl flex justify-center items-center mt-2'>
+                        <AiFillStar></AiFillStar>
+                        <AiFillStar></AiFillStar>
+                        <AiFillStar></AiFillStar>
+                        <AiFillStar></AiFillStar>
+                        <AiFillStar></AiFillStar>
+                    </div>
                 </div>
             </div>
 
